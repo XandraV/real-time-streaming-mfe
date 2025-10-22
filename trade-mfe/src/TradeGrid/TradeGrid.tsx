2873797c6e383e3eb884d82a0adb43cd2ba64c62
XandraV <veresalexandra95@gmail.com>
2025-10-22 18:20:27 +0100

    clean up

--- trade-mfe/src/TradeGrid/TradeGrid.tsx
+++ trade-mfe/src/TradeGrid/TradeGrid.tsx
@@ -7,12 +7,11 @@ import {
   themeAlpine,
   colorSchemeDarkBlue,
 } from "ag-grid-community";
-import { AgGridReact } from "ag-grid-react"; // React Data Grid Component
+import { AgGridReact } from "ag-grid-react";
 import { colDefs, defaultColDef } from "./columnDef";
 import type { RowsMap } from "../types";
 import { StyledWrapper } from "./StyledWrapper";
 
-// Register all Community features
 ModuleRegistry.registerModules([AllCommunityModule]);
 
 type GridProps = {
@@ -26,14 +25,7 @@ const TradeGrid = ({ rowsMap }: GridProps) => {
     []
   );
 
-  const myTheme = themeAlpine.withParams({
-    backgroundColor: "#1f2836",
-    foregroundColor: "rgba(255, 255, 255, 1)",
-    headerTextColor: "white",
-    headerBackgroundColor: "#2b3340",
-    headerColumnResizeHandleColor: "#4b525d",
-  });
-  const myTheme2 = themeAlpine.withPart(colorSchemeDarkBlue).withParams({
+  const myTheme = themeAlpine.withPart(colorSchemeDarkBlue).withParams({
     valueChangeValueHighlightBackgroundColor: "#823fcaa0",
     valueChangeDeltaDownColor: "rgb(255, 0, 92)",
     valueChangeDeltaUpColor: "rgb(53, 182, 90)",
@@ -43,7 +35,7 @@ const TradeGrid = ({ rowsMap }: GridProps) => {
     <StyledWrapper>
       <div style={{ height: 900 }}>
         <AgGridReact
-          theme={myTheme2}
+          theme={myTheme}
           rowData={Object.values(rowsMap)}
           columnDefs={colDefs}
           defaultColDef={defaultColDef}
