859fea1e2cf6cdc61e03a1476cd67acc3f8a6566
XandraV <veresalexandra95@gmail.com>
2025-12-01 14:31:00 +0000

    Revert "rever keep alive"
    This reverts commit cf3ac959d4e650cf9975701446910d09e191952a.

--- shell/src/App.tsx
+++ shell/src/App.tsx
@@ -1,38 +1,54 @@
-import React, { Suspense } from "react";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   Navigate,
+  useLocation,
 } from "react-router-dom";
+import React, { Suspense } from "react";
 import NavMenu from "./NavMenu";
 
 const RemoteTradeApp = React.lazy(() => import("trade/TradeApp"));
 const RemotePortfolioApp = React.lazy(() => import("portfolio/PortfolioApp"));
 
-function Home() {
-  return <h2 style={{ color: "white" }}>Welcome to Dashboard App</h2>;
-}
-function Watchlist() {
-  return <h2 style={{ color: "white" }}>Welcome to the Watchlist App</h2>;
+// <Routes> normally unmount on navigation ie local state is lost
+// Simple KeepAliveRoutes keeps components mounted and hence state is preserved
+function KeepAliveRoutes() {
+  const location = useLocation();
+  const route = location.pathname;
+
+  return (
+    <div>
+      <div style={{ display: route === "/trade" ? "block" : "none" }}>
+        <RemoteTradeApp />
+      </div>
+
+      <div style={{ display: route === "/portfolio" ? "block" : "none" }}>
+        <RemotePortfolioApp />
+      </div>
+
+      <div style={{ display: route === "/watchlist" ? "block" : "none" }}>
+        <h2 style={{ color: "white" }}>Welcome to the Watchlist App</h2>
+      </div>
+
+      <div style={{ display: route === "/dashboard" ? "block" : "none" }}>
+        <h2 style={{ color: "white" }}>Welcome to Dashboard App</h2>
+      </div>
+    </div>
+  );
 }
 
 function App() {
   return (
     <Router>
-      <div>
-        <NavMenu />
-        <Suspense fallback={<div style={{ color: "white" }}>Loading…</div>}>
-          <Routes>
-            <Route path="/" element={<Navigate to="/trade" replace />} />
-            {/* <Route path="/" element={<Home />} /> */}
-            <Route path="/dashboard" element={<Home />} />
-            <Route path="/trade" element={<RemoteTradeApp />} />
-            <Route path="/portfolio" element={<RemotePortfolioApp />} />
-            <Route path="/watchlist" element={<Watchlist />} />
-          </Routes>
-        </Suspense>
-      </div>
+      <NavMenu />
+      <Suspense fallback={<div style={{ color: "white" }}>Loading…</div>}>
+        <Routes>
+          <Route path="/" element={<Navigate to="/trade" replace />} />
+        </Routes>
+        {/* Keep-alive renderer */}
+        <KeepAliveRoutes />
+      </Suspense>
     </Router>
   );
 }
