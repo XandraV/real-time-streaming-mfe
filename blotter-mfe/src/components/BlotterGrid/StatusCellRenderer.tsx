c61a638b0c6e6629eb682145ad76f40996b09276
XandraV <veresalexandra95@gmail.com>
2025-12-06 10:39:32 +0000

    refactor

--- blotter-mfe/src/components/BlotterGrid/StatusCellRenderer.tsx
+++ blotter-mfe/src/components/BlotterGrid/StatusCellRenderer.tsx
@@ -1,22 +1,27 @@
 import React from "react";
 import CircleIcon from "@mui/icons-material/Circle";
+import styled from "styled-components";
 import type { ICellRendererParams } from "ag-grid-community";
 
 interface StatusCellRendererProps extends ICellRendererParams {
   value: string;
 }
 
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 4px;
+  margin-left: 20px;
+`;
+
 const StatusCellRenderer: React.FC<StatusCellRendererProps> = ({ value }) => {
-  let color = "#FFC107"; 
-  if (value === "Booked") color = "#4CAF50"; 
+  const color = value === "Booked" ? "#4CAF50" : "#FFC107";
 
   return (
-    <div
-      style={{ display: "flex", alignItems: "center", gap: 4, marginLeft: 20 }}
-    >
+    <Container>
       <CircleIcon style={{ color, fontSize: 12 }} />
       <span>{value}</span>
-    </div>
+    </Container>
   );
 };
 
