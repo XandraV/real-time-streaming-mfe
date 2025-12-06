e6d61b7deda9b04988f9a00c600c07c5a69ebce3
XandraV <veresalexandra95@gmail.com>
2025-12-06 11:30:55 +0000

    extracted keep alive component

--- shell/src/App.tsx
+++ shell/src/App.tsx
@@ -3,41 +3,10 @@ import {
   Routes,
   Route,
   Navigate,
-  useLocation,
 } from "react-router-dom";
-import React, { Suspense } from "react";
+import { Suspense } from "react";
 import Header from "./components/Header";
-
-import RemoteTradeApp from "trade/TradeApp";
-import RemotePortfolioApp from "portfolio/PortfolioApp";
-const RemoteBlotterApp = React.lazy(() => import("blotter/BlotterApp"));
-// <Routes> normally unmount on navigation ie local state is lost
-// KeepAliveRoutes keeps components mounted and hence state is preserved
-function KeepAliveRoutes() {
-  const location = useLocation();
-  const route = location.pathname;
-
-  return (
-    <div>
-      <div style={{ display: route === "/trade" ? "block" : "none" }}>
-        <RemoteTradeApp />
-        <RemoteBlotterApp />
-      </div>
-
-      <div style={{ display: route === "/portfolio" ? "block" : "none" }}>
-        <RemotePortfolioApp />
-      </div>
-
-      <div style={{ display: route === "/watchlist" ? "block" : "none" }}>
-        <h2 style={{ color: "white" }}>Welcome to the Watchlist App</h2>
-      </div>
-
-      <div style={{ display: route === "/dashboard" ? "block" : "none" }}>
-        <h2 style={{ color: "white" }}>Welcome to Dashboard App</h2>
-      </div>
-    </div>
-  );
-}
+import KeepAliveRoutes from "./KeepAliveRoutes";
 
 function App() {
   return (
---
+++ shell/src/KeepAliveRoutes.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import styled from "styled-components";
+import { useLocation } from "react-router-dom";
+
+import RemoteTradeApp from "trade/TradeApp";
+import RemotePortfolioApp from "portfolio/PortfolioApp";
+const RemoteBlotterApp = React.lazy(() => import("blotter/BlotterApp"));
+
+const RouteContainer = styled.div<{ show: boolean }>`
+  display: ${(props) => (props.show ? "block" : "none")};
+`;
+
+// <Routes> normally unmount on navigation ie local state is lost
+// KeepAliveRoutes keeps components mounted and hence state is preserved
+function KeepAliveRoutes() {
+  const { pathname } = useLocation();
+
+  return (
+    <div>
+      <RouteContainer show={pathname === "/trade"}>
+        <RemoteTradeApp />
+        <RemoteBlotterApp />
+      </RouteContainer>
+
+      <RouteContainer show={pathname === "/portfolio"}>
+        <RemotePortfolioApp />
+      </RouteContainer>
+
+      <RouteContainer show={pathname === "/watchlist"}>
+        <h2>Welcome to the Watchlist App</h2>
+      </RouteContainer>
+
+      <RouteContainer show={pathname === "/dashboard"}>
+        <h2>Welcome to Dashboard App</h2>
+      </RouteContainer>
+    </div>
+  );
+}
+
+export default KeepAliveRoutes;
